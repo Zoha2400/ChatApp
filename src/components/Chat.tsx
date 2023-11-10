e82a83e96909411fcbad3c65f31906263269241f
Zohir <vasv29834@gmail.com>
2023-11-10 09:49:39 +0500

    changed

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,14 +1,24 @@
 function Chat() {
   return (
     <div className="chat w-96 h-auto bg-stone-900 p-5 rounded-lg">
-      <div className="info flex flex-col gap-1 mb-2 text-xs">
-        <p className="room w-full box-border">Room: </p>
-        <p className="user">User: </p>
+      <div className="info flex flex-col gap-3 mb-4 text-xs">
+        <p className="room w-full box-border">
+          Room:{" "}
+          <span className="p-1 bg-green-500 rounded-md">x656sadjkWijsd</span>{" "}
+        </p>
+        <p className="user">
+          User:{" "}
+          <span className="p-1 bg-purple-500 rounded-md">x656sadjkWijsd</span>
+        </p>
       </div>
-      <div className="chat-disp w-full h-96 rounded-md bg-stone-950"></div>
+      <div className="chat-disp w-full h-96 rounded-md bg-stone-950 relative">
+        <p className="absolute w-full h-full flex justify-center items-center text-3xl font-extrabold opacity-25">
+          Chat
+        </p>
+      </div>
       <div className="flex items-end justify-center mt-2 gap-2 flex-col">
         <textarea
-          className=" resize-none h-16 outline-none border rounded-xl p-1 w-full bg-indigo-900 text-xs"
+          className=" resize-none h-16 outline-none border rounded-xl p-1 w-full bg-stone-950 text-xs"
           name="message"
           placeholder="Type a message"
           id="msg"
